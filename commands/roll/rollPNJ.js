bda396b265fd5ef9b5fa48c32833ea0c396b7063
tlunel <tlunel@greensystemes.com>
2024-05-16 13:51:23 +0200

    change rollPNJ

--- commands/roll/rollPNJ.js
+++ commands/roll/rollPNJ.js
@@ -7,6 +7,7 @@ const {
   getPricipale,
   getAllPNJ,
 } = require("../../utils/manipulerjson");
+const { adminId } = require("../../config.json");
 const { jetDe, jetCritique } = require("../../utils/diceFunction");
 const { musiquetime } = require("../../utils/vocalFunction");
 
@@ -33,74 +34,84 @@ module.exports = {
         .setAutocomplete(true),
     ),
   async autocomplete(interaction) {
-    const focusedOption = interaction.options.getFocused(true);
-    if (focusedOption.name === "attribut") {
-      let idJoueur = interaction.user.id;
-      message = interaction.options.data[0].value;
-      const focusedValue = interaction.options.getFocused();
-      const choices = getPersoAllPNJ(message);
-      const filtered = choices.filter((choice) =>
-        choice.includes(focusedValue),
-      );
-      await interaction.respond(
-        filtered.map((choice) => ({ name: choice, value: choice })),
-      );
-    } else {
-      message = interaction.options.data[1].value;
-      const focusedValue = interaction.options.getFocused();
-      const choices = getAllPNJ(message);
-      await interaction.respond(
-        choices.map((choice) => ({ name: choice, value: choice })),
-      );
+    const userId = interaction.user.id;
+    if (userId === adminId) {
+      const focusedOption = interaction.options.getFocused(true);
+      if (focusedOption.name === "attribut") {
+        let idJoueur = interaction.user.id;
+        message = interaction.options.data[0].value;
+        const focusedValue = interaction.options.getFocused();
+        const choices = getPersoAllPNJ(message);
+        const filtered = choices.filter((choice) =>
+          choice.includes(focusedValue),
+        );
+        await interaction.respond(
+          filtered.map((choice) => ({ name: choice, value: choice })),
+        );
+      } else {
+        message = interaction.options.data[1].value;
+        const focusedValue = interaction.options.getFocused();
+        const choices = getAllPNJ(message);
+        await interaction.respond(
+          choices.map((choice) => ({ name: choice, value: choice })),
+        );
+      }
     }
   },
   async execute(interaction) {
     const userId = interaction.user.id;
-    //les attributs de la fonction
-    let attribut = interaction.options.getString("attribut");
-    let bonus = interaction.options.getInteger("bonus");
-    let pnj = interaction.options.getString("pnj");
+    if (userId === adminId) {
+      //les attributs de la fonction
+      let attribut = interaction.options.getString("attribut");
+      let bonus = interaction.options.getInteger("bonus");
+      let pnj = interaction.options.getString("pnj");
 
-    if (typeof attribut === "string" && attribut[0] === " ") {
-      attribut = attribut.slice(1);
-    }
-    //la valeur de la stat
-    if (pnj === null) {
-      pnj = getPricipale();
-    }
-    const valAttribut = valeurAttributPNJ(attribut, pnj);
-    let random = jetDe();
-    const randomCritique = jetCritique();
-    let message = "";
-
-    //on gere les critique
-    if (random === 10) {
-      message += "REUSSITE CRITIQUE !!! : +" + randomCritique + "\n";
-      random += randomCritique;
-      musiquetime(chercheMusiqueVocal(userId), 30000);
-    }
-    if (random === 1) {
-      message = "échec critique ... : -" + randomCritique + "\n";
-      random -= randomCritique;
-      musiquetime("./musique/echec.mp3", 5000);
-    }
-    let res = 0;
-    if (valAttribut !== undefined) {
-      message += "jet de " + attribut + " : " + random + " + " + valAttribut;
-
-      if (bonus !== null) {
-        message += "+ " + bonus;
-        random = random + bonus;
+      if (typeof attribut === "string" && attribut[0] === " ") {
+        attribut = attribut.slice(1);
       }
-      res = random + parseInt(valAttribut);
-      message += " = " + res;
-    } else {
-      res = random;
-      message += "jet simple :" + random;
+      //la valeur de la stat
+      if (pnj === null) {
+        pnj = getPricipale();
+      }
+      const valAttribut = valeurAttributPNJ(attribut, pnj);
+      let random = jetDe();
+      const randomCritique = jetCritique();
+      let message = "";
+
+      //on gere les critique
+      if (random === 10) {
+        message += "REUSSITE CRITIQUE !!! : +" + randomCritique + "\n";
+        random += randomCritique;
+        musiquetime(chercheMusiqueVocal(userId), 30000);
+      }
+      if (random === 1) {
+        message = "échec critique ... : -" + randomCritique + "\n";
+        random -= randomCritique;
+        musiquetime("./musique/echec.mp3", 5000);
+      }
+      let res = 0;
+      if (valAttribut !== undefined) {
+        message += "jet de " + attribut + " : " + random + " + " + valAttribut;
+
+        if (bonus !== null) {
+          message += "+ " + bonus;
+          random = random + bonus;
+        }
+        res = random + parseInt(valAttribut);
+        message += " = " + res;
+      } else {
+        res = random;
+        message += "jet simple :" + random;
+      }
+      console.log(message);
+      await interaction.reply(
+        pnj + " => jet de " + attribut + " : **" + res + "**",
+      );
     }
-    console.log(message);
-    await interaction.reply(
-      pnj + " => jet de " + attribut + " : **" + res + "**",
-    );
+    else
+    {
+      await interaction.reply("vous n'avez pas les droits");
+    }
+
   },
 };
