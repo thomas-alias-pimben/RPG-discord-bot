c58f7050db1711cc865c722bb33c988068459387
tlunel <tlunel@greensystemes.com>
2024-05-16 11:32:08 +0200

    test boutton

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,8 +5,11 @@ module.exports = {
     .setName("ping")
     .setDescription("ping ... pong LOL"),
   async execute(interaction) {
-    await interaction.reply(
-      "Pong! \n Websocket heartbeat:" + interaction.client.ws.ping,
+    await interaction.reply({
+      content: "Pong! \n Websocket heartbeat:" + interaction.client.ws.ping,
+      ephemeral: true,
+    }
     );
+    console.log(interaction.user.username+" à utiliser ping : Websocket heartbeat:" + interaction.client.ws.ping);
   },
 };
