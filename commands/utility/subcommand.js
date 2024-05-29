c21e4814c2e2fcc11e15a3195e84c4f5fd036ece
tlunel <tlunel@greensystemes.com>
2024-05-29 16:13:06 +0200

    mise à jour

--- commands/utility/subcommand.js
+++ commands/utility/subcommand.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const config = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,6 +17,20 @@ module.exports = {
       subcommand.setName("server").setDescription("Info about the server"),
     ),
   async execute(interaction) {
+
+    const Tenor = require("tenorjs").client({
+      "Key": config.TenorKey, // https://developers.google.com/tenor/guides/quickstart
+      "Filter": "off", // "off", "low", "medium", "high", not case sensitive
+      "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+      "MediaFilter": "minimal", // either minimal or basic, not case sensitive
+      "DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
+    });
+    Tenor.Search.Query("hug", "3").then(Results => {
+      Results.forEach(Post => {
+        console.log(`Item #${Post.id} (Created: ${Post.created}) @ ${Post.url}`);
+      });
+    }).catch(console.error);
+
     await interaction.reply(interaction.options.getSubcommand());
   },
 };
