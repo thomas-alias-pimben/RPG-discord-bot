6e50699cee5c06f8d5bdb41574a14cca25c34af9
pimben <lunelthomas974@gmail.com>
2026-02-12 17:52:54 +0100

    transformer petit à petit les commande mj en commande normal, en résteignant certaines fonctionnalité à l'admin seulement

--- commands/change_pv_ps/voirPv.js
+++ commands/change_pv_ps/voirPv.js
@@ -1,14 +1,38 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { affPv } = require("../../utils/manipulerjson");
+const { MessageFlags } = require('discord.js');
+
+const { adminId } = require("../../config.json");
+
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("voir_pv_ps")
-    .setDescription("affiches tes PV et PS"),
+    .setDescription("affiches tes PV et PS").addUserOption((option) =>
+          option.setName("user").setDescription("l'utilisateur"),
+        ),
   async execute(interaction) {
     //les attributs de la fonction
     userId = interaction.user.id;
+    admin=false
+
+
+
+    if(interaction.options.getUser("user") !== null )
+    {
+      userId = interaction.options.getUser("user").id;
+      admin=true;
+    }
+
+    if (admin && interaction.user.id !== adminId) {
+       await interaction.reply({
+      content: "MON PETIT!! TU N'EST PAS ADMIN",
+      ephemeral: true,
+    });
+      } else {
+
     const message = "```" + affPv(userId) + "```";
-    await interaction.reply({ content: "" + affPv(userId), ephemeral: true });
+    await interaction.reply({ content: "" + affPv(userId), flags: MessageFlags.Ephemeral });
+      }
   },
 };
