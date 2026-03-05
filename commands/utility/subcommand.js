545d834a2bc4d9cde5174354ad9a89cba02332c7
pimben <lunelthomas974@gmail.com>
2026-03-05 12:22:40 +0100

    Update subcommand.js

--- commands/utility/subcommand.js
+++ commands/utility/subcommand.js
@@ -1,5 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const config = require("../../config.json");
+const { ChannelType } = require('discord.js');
+const { MessageFlags } = require('discord.js');
+
+const {
+  afficherPlusieursPartie,
+} = require("../../utils/manipulerjson");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,16 +16,60 @@ module.exports = {
         .setName("user")
         .setDescription("Info about a user")
         .addUserOption((option) =>
-          option.setName("target").setDescription("The user"),
+          option.setName("target").setDescription("The user").setRequired(true),
         ),
     )
     .addSubcommand((subcommand) =>
       subcommand.setName("server").setDescription("Info about the server"),
     ),
   async execute(interaction) {
+   
+   
     if(interaction.options.getSubcommand() === "server")
     {
-      await interaction.reply(interaction.options.getSubcommand());
+      
+      text = "nom:"+interaction.guild.name+"\n"
+      text +="les channels\n"
+
+      i=1
+      //savoir si le channel est public
+       interaction.guild.channels.cache.forEach(channel => {
+        const everyoneRole = channel.guild.roles.everyone;
+        const canEveryoneView = channel
+                                .permissionsFor(everyoneRole)
+                                .has('ViewChannel');
+        
+        
+        if (canEveryoneView && channel.type !== ChannelType.GuildCategory)
+        {
+          text += "\n\tchannel " + i +" : "+channel.name+"\n"
+          text += "\t\tid:"+ channel.id+"\n"
+          text += "\t\ttype:"+ ChannelType[channel.type]+"\n"
+          
+         i++
+        }
+      });
+      
+      
+     
+      
+      
+      partie = afficherPlusieursPartie(text)
+
+      await interaction.reply({
+            content: "```" + partie[0] + "```",
+            flags: MessageFlags.Ephemeral,
+          });
+          if (partie.length > 1) {
+            partie.shift();
+            for (const element of partie) {
+              await interaction.followUp({
+                content: "```" + element + "```",
+                flags: MessageFlags.Ephemeral,
+              });
+            }
+          }
+      
     }
     if(interaction.options.getSubcommand() === "user")
     {
@@ -32,8 +82,15 @@ module.exports = {
       else
       {
         const member = interaction.guild.members.cache.get(user.id)
-        await interaction.reply("c'est " + member.nickname + " !\npseudo: "+ user.globalName  );
-      }
+        if(member.nickname !== null)
+        {  
+            await interaction.reply("c'est " + member.nickname + " !\npseudo: "+ user.globalName  );
+        }
+        else
+          {
+            await interaction.reply("c'est " + user.globalName  );
+          }
+        }
       
     }
     
